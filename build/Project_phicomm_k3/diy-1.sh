2bf14d548264c7aa4b256a819147694fe1a4efc2
281677160 <66651997+281677160@users.noreply.github.com>
2020-10-27 19:37:29 +0800

    Update diy-1.sh

--- build/Project_phicomm_k3/diy-1.sh
+++ build/Project_phicomm_k3/diy-1.sh
@@ -8,5 +8,4 @@
 #源码里面几乎囊括了所有插件，所以基本不需要添加插件了
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
-#全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
-#用WinSCP连接openwrt，在/www/luci-static/argon里面创建background文件夹（如果本身就有background就不需要创建）来存放jpg png gif格式图片可以自定义登陆界面，gif图片为动态登陆界面
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config  #argon主题设置（在固件的系统里面）
