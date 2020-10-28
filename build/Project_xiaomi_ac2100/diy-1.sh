5bf816c70ee069274a1fae6d5ce3a20d328902da
281677160 <66651997+281677160@users.noreply.github.com>
2020-10-28 15:04:34 +0800

    Update diy-1.sh

--- build/Project_xiaomi_ac2100/diy-1.sh
+++ build/Project_xiaomi_ac2100/diy-1.sh
@@ -5,4 +5,6 @@
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
 
 
-#源码里面几乎囊括了所有插件，所以基本不需要添加插件了
+#源码里面几乎囊括了所有插件，所以基本不需要添加
+rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config  #argon主题设置（在固件的系统里面）
