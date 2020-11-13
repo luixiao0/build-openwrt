22c250947b93ce4e84216ed2c6c6d790e335ab80
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-13 12:11:16 +0800

    Update diy-1.sh

--- build/Lede_phicomm_k2p/diy-1.sh
+++ build/Lede_phicomm_k2p/diy-1.sh
@@ -1,6 +1,7 @@
-﻿#!/bin/bash
+#!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
+
 
 # 使用源码自带ShadowSocksR Plus+出国软件
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
