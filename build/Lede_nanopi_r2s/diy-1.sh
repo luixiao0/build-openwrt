76b4b277d55b3fb1c91cd0f5c6b285e27ff730d8
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-13 12:13:33 +0800

    Update diy-1.sh

--- build/Lede_nanopi_r2s/diy-1.sh
+++ build/Lede_nanopi_r2s/diy-1.sh
@@ -2,11 +2,11 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-
 # 使用源码自带ShadowSocksR Plus+出国软件
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/diy/luci-theme-rosy  #主题-rosy
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge  #主题-edge-动态登陆界面
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom  #透明主题
 git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash  #openclash出国软件
