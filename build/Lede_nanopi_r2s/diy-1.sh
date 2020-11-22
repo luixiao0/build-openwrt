318dc15c4f33da8146f72296aa60d16bc7e9eb06
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-22 22:11:21 +0800

    Update diy-1.sh

--- build/Lede_nanopi_r2s/diy-1.sh
+++ build/Lede_nanopi_r2s/diy-1.sh
@@ -2,36 +2,8 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-# 插件包地址
-#https://github.com/281677160/openwrt-package
-
-#源码已添加以下插件，所有插件都来自各插件作者源码，每天插件包同步更新
-#最好不要自己胡乱的添加别人的插件包，插件相同添加有增加编译失败风险
-#如果这里没有你需要的插件，添加的时候单独添加就好了，不要去拉取别人的插件包，或者告诉我，我在我插件包添加上
-
-#luci-theme-rosy  #主题-rosy
-#luci-theme-edge  #主题-edge
-#luci-theme-opentomcat  #主题-opentomcat
-#luci-theme-opentopd  #主题-opentopd
-#luci-theme-rosy  #主题-rosy
-#luci-theme-infinityfreedom  #透明主题
-#luci-app-openclash  #openclash 出国软件
-#luci-app-clash  #clash 出国软件
-#luci-app-serverchan  #微信推送
-#luci-app-eqos  #内网控速 内网IP限速工具
-#luci-app-jd-dailybonus  #京东签到
-#luci-app-passwall  #passwall 出国软件
-#luci-app-poweroff  #关机（增加关机功能）
-#luci-theme-argon  #新的argon主题
-#luci-app-argon-config   #argon主题设置（编译时候选上,在固件的‘系统’里面）
-#luci-app-k3screenctrl  #k3屏幕，k3路由器专用
-#luci-app-koolproxyR  #广告过滤大师 plus+  慎用，不懂的话，打开就没网络了
-#luci-app-oaf  #应用过滤 该模块只工作在路由模式， 旁路模式、桥模式不生效
-#luci-app-ssr-plus  #shadowsocksR Puls+  出国软件
-#luci-app-vssr  #Hello World 也叫彩旗飘飘  出国软件
-#luci-app-gost  #GO语言实现的安全隧道
-#luci-app-aliddns  #阿里DDNS
-#luci-app-smartdns  #smartdns DNS加速
-#luci-app-adguardhome  #adguardhome
-#luci-app-dockerman  #docker容器
-
+# 基本不需要添加啥插件了,我搜集了各位大神的插件都集成一个软件包直接打入源码里面了
+# 要了解增加了什么东西，就到我的专用软件包里面看看看吧，如果还是没有你需要的插件，请不要一下子就拉取别人的插件包
+# 相同的文件都拉一起，因为有一些可能还是其他大神修改过的容易造成编译错误的
+# 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
+# 软件包地址：https://github.com/281677160/openwrt-package
