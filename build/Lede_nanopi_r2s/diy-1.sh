9eb116f06dffad82b8039abbe5abacf39867e56a
281677160 <66651997+281677160@users.noreply.github.com>
2020-11-22 16:54:00 +0800

    Update diy-1.sh

--- build/Lede_nanopi_r2s/diy-1.sh
+++ build/Lede_nanopi_r2s/diy-1.sh
@@ -8,9 +8,9 @@
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 #luci-theme-rosy  #主题-rosy
 #luci-theme-edge  #主题-edge
-#luci-theme-opentomcat #主题-opentomcat
-#luci-theme-opentopd #主题-opentopd
-#luci-theme-rosy #主题-rosy
+#luci-theme-opentomcat  #主题-opentomcat
+#luci-theme-opentopd  #主题-opentopd
+#luci-theme-rosy  #主题-rosy
 #luci-theme-infinityfreedom  #透明主题
 #luci-app-openclash  #openclash 出国软件
 #luci-app-clash  #clash 出国软件
@@ -20,14 +20,14 @@
 #luci-app-passwall  #passwall 出国软件
 #luci-app-poweroff  #关机（增加关机功能）
 #luci-theme-argon  #新的argon主题
-#luci-app-argon-config  #argon主题设置（编译时候选上,在固件的‘系统’里面）
-#luci-app-k3screenctrl #k3屏幕，k3路由器专用
-#luci-app-koolproxyR #广告过滤大师 plus+  慎用，不懂的话，打开就没网络了
-#luci-app-oaf #应用过滤 该模块只工作在路由模式， 旁路模式、桥模式不生效
-#luci-app-ssr-plus #shadowsocksR Puls+  出国软件
-#luci-app-vssr #Hello World 也叫彩旗飘飘  出国软件
-#luci-app-gost #GO语言实现的安全隧道
+#luci-app-argon-config   #argon主题设置（编译时候选上,在固件的‘系统’里面）
+#luci-app-k3screenctrl  #k3屏幕，k3路由器专用
+#luci-app-koolproxyR  #广告过滤大师 plus+  慎用，不懂的话，打开就没网络了
+#luci-app-oaf  #应用过滤 该模块只工作在路由模式， 旁路模式、桥模式不生效
+#luci-app-ssr-plus  #shadowsocksR Puls+  出国软件
+#luci-app-vssr  #Hello World 也叫彩旗飘飘  出国软件
+#luci-app-gost  #GO语言实现的安全隧道
 #luci-app-smartdns  #smartdns DNS加速
-#luci-app-adguardhome #adguardhome
-#luci-app-dockerman #docker容器
+#luci-app-adguardhome  #adguardhome
+#luci-app-dockerman  #docker容器
 
