e639fa1bcced53bd5f5d5f2bd6425f74f0f064e9
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-01 12:34:13 +0800

    Update diy-2.sh

--- build/openwrt/diy-2.sh
+++ build/openwrt/diy-2.sh
@@ -7,6 +7,8 @@
 #
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+# 请不要直接套用我其他机型的扩展或者插件，每个源码有些文件路径都多多少少不一样，直接套用会出错
+#
+#
 # 修改网关（把192.168.2.2改成你需要的就行）
 #sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
