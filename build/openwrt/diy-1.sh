c666343eca29d8ffeaee6a1d7f76b0ca099cbe21
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-01 12:33:27 +0800

    Update diy-1.sh

--- build/openwrt/diy-1.sh
+++ build/openwrt/diy-1.sh
@@ -7,7 +7,8 @@
 #
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
+#请不要直接套用我其他机型的扩展或者插件，每个源码有些文件路径都多多少少不一样，直接套用会出错
+#
 # lede源码增加ShadowSocksR Plus+
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
