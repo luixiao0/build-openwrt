5fa77cc5bb4c46abb8989bf32f983b0172db8a49
281677160 <66651997+281677160@users.noreply.github.com>
2020-12-18 12:21:09 +0800

    Update diy-2.sh

--- build/Lede_wndr3800/diy-2.sh
+++ build/Lede_wndr3800/diy-2.sh
@@ -12,7 +12,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' p
 sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
