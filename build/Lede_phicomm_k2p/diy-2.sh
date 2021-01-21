4686e9390fa0f369010763c7fd2debb5a29ed84a
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-21 17:58:11 +0800

    Update diy-2.sh

--- build/Lede_phicomm_k2p/diy-2.sh
+++ build/Lede_phicomm_k2p/diy-2.sh
@@ -11,13 +11,11 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' p
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-# 小闪存机子建议不使用这个，用了这个主题后会占用0.4G了，对小闪存机子相当不友好
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）(小闪存机子建议不使用)
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 如果你的K2P支持31MB闪存的请开启，默认K2P固件不能超过15.3MB的，超过就不能刷机了
 #sed -i 's/15744k/32448k/g' target/linux/ramips/image/mt7621.mk
-
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
