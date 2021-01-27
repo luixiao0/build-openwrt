f8dccfd0540ba9fe7db0171152f4079e08824a54
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-28 00:59:19 +0800

    Update diy-2.sh

--- build/Lienol_phicomm_k2p/diy-2.sh
+++ build/Lienol_phicomm_k2p/diy-2.sh
@@ -14,7 +14,7 @@ sed -i "s/OpenWrt /281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" pa
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）（小闪存机子不建议使用）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 如果你的K2P支持32MB闪存的请开启，默认K2P固件不能超过15.3MB的，超过就不能刷机了（如果有其他mt7621芯片一起编译要注意,打开这个会把整个文件的16m改成32m）
+# 如果你的K2P支持32MB闪存的请开启，默认K2P固件不能超过15.3MB的（如果有其他mt7621芯片一起编译要注意,打开这个会把整个文件的16m改成32m）
 #sed -i 's/ralink_default_fw_size_16M/ralink_default_fw_size_32M/g' target/linux/ramips/image/mt7621.mk
 
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
