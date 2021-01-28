6c3b7defdd7dde5226ff8274aaa1b88f2eac5733
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-28 11:26:17 +0800

    Update diy-1.sh

--- build/openwrt/diy-1.sh
+++ build/openwrt/diy-1.sh
@@ -9,8 +9,5 @@
 #
 # 请不要直接套用我其他机型的扩展或者插件，每个源码有些文件路径都多多少少不一样，直接套用会出错
 #
-# lede源码增加ShadowSocksR Plus+
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# lede源码增加passwall,也适用于增加其他插件的方法
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
