a5bfd72b1e4a033478348a19b89cc3bab23b4741
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-17 19:19:15 +0800

    Update common.sh

--- common/common.sh
+++ common/common.sh
@@ -1,74 +1,65 @@
-Diy_lede() {
+Diy_all() {
+echo "全部源码通用"
 rm -rf package/lienol/luci-app-timecontrol
 rm -rf package/ctcgfw/{luci-app-argon-config,luci-theme-argonv3}
 rm -rf package/lean/{luci-app-netdata,luci-theme-argon,k3screenctrl}
-
 git clone -b $REPO_BRANCH --single-branch https://github.com/281677160/openwrt-package package/danshui
 svn co https://github.com/281677160/openwrt-package/branches/usb/AutoUpdate package/base-files/files/bin
 chmod +x package/base-files/files/bin/* ./
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
+}
 
+#####
+
+Diy_lede() {
+echo "LEDE源码使用"
 git clone https://github.com/fw876/helloworld package/danshui/luci-app-ssr-plus
 git clone https://github.com/xiaorouji/openwrt-passwall package/danshui/luci-app-passwall
 git clone https://github.com/jerrykuku/luci-app-vssr package/danshui/luci-app-vssr
 git clone https://github.com/vernesong/OpenClash package/danshui/luci-app-openclash
 }
 
+#####
+
 Diy_lienol() {
-rm -rf package/lienol/luci-app-timecontrol
-rm -rf package/ctcgfw/{luci-app-argon-config,luci-theme-argonv3}
-rm -rf package/lean/{luci-app-netdata,luci-theme-argon,k3screenctrl}
-
-git clone -b $REPO_BRANCH --single-branch https://github.com/281677160/openwrt-package package/danshui
-svn co https://github.com/281677160/openwrt-package/branches/usb/AutoUpdate package/base-files/files/bin
-chmod +x package/base-files/files/bin/* ./
-
+echo "LIENOL源码使用"
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 }
 
-Diy_immortalwrt() {
-rm -rf package/lienol/luci-app-timecontrol
-rm -rf package/ctcgfw/{luci-app-argon-config,luci-theme-argonv3}
-rm -rf package/lean/{luci-app-netdata,luci-theme-argon,k3screenctrl}
+#####
 
-git clone -b $REPO_BRANCH --single-branch https://github.com/281677160/openwrt-package package/danshui
-svn co https://github.com/281677160/openwrt-package/branches/usb/AutoUpdate package/base-files/files/bin
-chmod +x package/base-files/files/bin/* ./
+Diy_immortalwrt() {
+echo "天灵源码使用"
 }
 
 ######################################################################################################
 
 Diy_lede2() {
+echo "LEDE源码使用"
 curl -fsSL  https://raw.githubusercontent.com/xiaorouji/openwrt-passwall/main/v2ray-plugin/Makefile > package/lean/v2ray-plugin/Makefile
 rm -rf {LICENSE,README,README.md}
 rm -rf ./*/{LICENSE,README,README.md}
 rm -rf ./*/*/{LICENSE,README,README.md}
-git clone https://github.com/openwrt-dev/po2lmo.git
-pushd po2lmo
-make && sudo make install
-popd
 }
 
 Diy_lienol2() {
+echo "LIENOL源码使用"
 rm -rf {LICENSE,README,README.md}
 rm -rf ./*/{LICENSE,README,README.md}
 rm -rf ./*/*/{LICENSE,README,README.md}
-git clone https://github.com/openwrt-dev/po2lmo.git
-pushd po2lmo
-make && sudo make install
-popd
 }
 
 Diy_immortalwrt2() {
+echo "天灵源码使用"
 rm -rf {LICENSE,README,README.md}
 rm -rf ./*/{LICENSE,README,README.md}
 rm -rf ./*/*/{LICENSE,README,README.md}
-git clone https://github.com/openwrt-dev/po2lmo.git
-pushd po2lmo
-make && sudo make install
-popd
 }
 
 
