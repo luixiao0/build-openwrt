65df8dc410388efbf058f25b58ed3a552196d1a0
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-17 07:20:45 +0800

    Update diy-2.sh

--- build/Project_newifi_d2/diy-2.sh
+++ build/Project_newifi_d2/diy-2.sh
@@ -8,6 +8,9 @@ sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generat
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-123'' package/lean/default-settings/files/zzz-default-settings
 
+# 版本号里显示一个自己的名字（把 ${Author} 改成你自己名字就行了，不改的话，默认使用你仓库帐号）
+sed -i "s/PTION='%D %V %C'/PTION='Compiled by ${Author} on $(TZ=UTC-8 date "+%Y/%m/%d") @ %D %V'/g" package/base-files/files/etc/openwrt_release
+
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
