bd2363780f64c6192197705260868d3df0075790
lux <43348732+luixiao0@users.noreply.github.com>
2021-07-04 01:56:48 +0800

    Update diy-2.sh

--- build/Project_x86_64/diy-2.sh
+++ build/Project_x86_64/diy-2.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
+# 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
